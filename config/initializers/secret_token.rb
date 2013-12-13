d0c5a67f00fe63c29798d8268806eb76b9a696db
Marcus Hayes <mhayes@chicagotechacademy.org>
2013-12-13 14:32:48 -0600

    initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '986ba747c2387c7205fc9ea9f6ce85bde32fedbb00001221b8ca2ee122bb1d8cf864d283e94aed1ac6e95c71bfcac43cdb66c11f3b14131c2986f05997bdacc9'
+FirstApp::Application.config.secret_token = 'e103af609fe3bdf8093ed1b986daa6739adcf29a15d1f414ba7bd446be6a664e407b7575db9b27d4a79c878eef1df183a87e1e0bb37530295e006d0b2df48fee'
